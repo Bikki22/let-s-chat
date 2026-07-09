65932cd79bb09e0746df94fef661db8405b53cf2
Bikki chaudhary <cbikki35@gmail.com>
2026-07-09 12:14:28 +0545

    feat: integrate Clerk for authentication and update dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import "./App.css";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
 const App = () => {
-  return <div>Hello guys</div>;
+  return (
+    <div>
+      <h1>Hello guys how are you this is my app</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </div>
+  );
 };
 
 export default App;
