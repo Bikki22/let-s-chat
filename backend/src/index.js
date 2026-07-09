7ed7b5845dce645653500cffd03a891fca22dc68
Bikki chaudhary <cbikki35@gmail.com>
2026-07-09 13:35:56 +0545

    feat: add Dockerfile and .dockerignore; implement Clerk webhook handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,18 @@ import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(express.json());
 app.use(
